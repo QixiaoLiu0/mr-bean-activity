0925ee631edb85b9105a41955009db0c33034994
Qixiao Liu <christal.liu008@gmail.com>
2026-01-16 09:45:30 -0700

    mr bean inin

---
+++ src/app/component/header.js
@@ -0,0 +1,14 @@
+export default function Header() {
+  // return (
+  //   <header className="flex gap-4 justify-around py-4">
+  //     <div>Logo</div>
+  //     <nav>
+  //       <ul>
+  //         <li>1</li>
+  //         <li>2</li>
+  //         <li>3</li>
+  //       </ul>
+  //     </nav>
+  //   </header>
+  // );
+}
